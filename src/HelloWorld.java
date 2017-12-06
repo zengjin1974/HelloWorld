b943dc23ba1981dbe358f325d6021fa06d3c1be5
zengjin <zengjin@G08CNXNBFS01880.bfs.cn.fujitsu.com>
2017-12-06 09:26:45 +0800

    20171206

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello, world!");
 		System.out.println("This is my first Git project!");
 		System.out.println("Everything goes well!");
+		System.out.println("I can run in FJ-WAN!");
 	}
 
 }
